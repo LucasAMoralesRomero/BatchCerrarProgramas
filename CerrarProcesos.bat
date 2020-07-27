f8992b68e88a4c29603d622d4e68a385dc2bf9d2
Lucas Agustín Morales Romero <moralesromerolucasagustin@gmail.com>
2020-07-27 07:33:13 -0300

    Update CerrarProcesos.bat
    Updates of the day:
    DB Report Engine close added (to avoid database disconnection errors)
    Add close of Open Office, Java Update (jucheck, jusched)
    ----------------------------------------------------------------------------------------------------------
    Actualizaciones del día:
    Se agrega cierre de Motor de Fondo de Base de Datos (para evitar error por desconexión de DB)
    Se agrega cierre de Open Office, Actualización de Java (jucheck, jusched)

--- CerrarProcesos.bat
+++ CerrarProcesos.bat
@@ -13,18 +13,21 @@ echo Seleccione la opcion deseada:
 echo.
 echo 1. Cerrar Ifrun (GDM, Etiquetas y Motor DB)
 echo 2. Cerrar Internet Explorer (en caso de error Electro)
-echo 3. Cerrar Programa
+echo 3. Cerrar programas para abrir GESCOM (Jusched, Jucheck, Soffice.bin, Soffice.exe)
+echo 4. Cerrar Programa
 echo.
 set /p var=
 if %var%==1 goto :Ifrun
 if %var%==2 goto :Iexplore
-if %var%==3 goto :Exit
-if %var% GTR 3 echo Error
+if %var%==3 goto :JavaExecute
+if %var%==4 goto :Exit
+if %var% GTR 4 echo Error
 goto :Menu
 :Ifrun
 cls
 color 5F
 taskkill /im ifrun60.exe /f /t
+taskkill /im RWRBE60.exe /f /t
 echo Presione una tecla para regresar al menu
 pause>null
 goto :Menu
@@ -35,6 +38,15 @@ taskkill /im iexplore.exe /f /t
 echo Presione una tecla para regresar al menu
 pause>null
 goto :Menu
+:JavaExecute
+cls
+color 5F
+taskkill /im jucheck.exe /f /t
+taskkill /im jusched.exe /f /t
+taskkill /im soffice.bin /f /t
+taskkill /im soffice.exe /f /t
+echo Presione una tecla para regresar al menu
+pause>null
 :Exit
 echo Para salir presiona una tecla.
 pause>nul
